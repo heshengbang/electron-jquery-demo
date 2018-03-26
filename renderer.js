8e7cfecd1536247cbaf7b7a7d846a55159b65e9b
heshengbang <yunshixin8521894@sohu.com>
2018-03-26 17:53:45 +0800

    add project file

---
+++ renderer.js
@@ -0,0 +1,10 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+// const newWindowBtn = document.getElementById('new-window')
+
+// newWindowBtn.addEventListener('click', (event) => {
+//   console.log("fuck you");
+// });
+console.log("1");
